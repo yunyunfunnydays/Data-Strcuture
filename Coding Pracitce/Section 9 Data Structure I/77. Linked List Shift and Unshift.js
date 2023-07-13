b913c46264ae14eec8703c2d274cf54fa686282e
yunhsuan <yunhsuan2017@gmail.com>
2023-07-13 17:36:34 +0800

    Unshift

---
+++ Coding Pracitce/Section 9 Data Structure I/77. Linked List Shift and Unshift.js
@@ -0,0 +1,100 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    this.length++;
+  }
+
+  // 最後值一個刪除
+  pop() {
+    if (!this.head) {
+      return null;
+    } else if (this.length === 1) {
+      let temp = this.head;
+      this.head = null;
+      this.length = 0;
+      return temp;
+    } else {
+      let currentNode = this.head;
+      for (let i = 1; i <= this.length - 2; i++) {
+        currentNode = currentNode.next;
+      }
+      let temp = currentNode.next;
+      currentNode.next = null;
+      this.length--;
+      return temp;
+    }
+  }
+
+  // 最前面的值刪除
+  shift() {
+    if (!this.head) {
+      return null
+    } else if (this.length === 1) {
+      let temp = this.head;
+      this.head = null;
+      this.length = 0;
+      return temp;
+    } else {
+      let temp = this.head;
+      this.head = temp.next;
+      this.length--;
+      return temp;
+    }
+  }
+
+  // 在最前面加入值
+  unshift(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    } else {
+      let temp = this.head;
+      let newNode = new Node(value);
+      this.head = newNode;
+      newNode.next = temp;
+    }
+    this.length++;
+  }
+
+  printAll() {
+    if (this.length === 0) {
+      console.log("Nothing in this linked list.");
+      return;
+    }
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+let myLinkedList = new LinkedList();
+myLinkedList.push("Mike");
+myLinkedList.push("Harry");
+myLinkedList.push("James");
+myLinkedList.push("Kevin");
+myLinkedList.pop();
+myLinkedList.shift();
+myLinkedList.unshift("Nora");
+myLinkedList.printAll();
