15a6ffad8c6a288a50a15347b98dc2391771ffb5
yunhsuan <yunhsuan2017@gmail.com>
2023-07-13 17:13:42 +0800

    75,76

---
+++ Coding Pracitce/Section 9 Data Structure I/75. Linked List Push.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    this.length++;
+  }
+
+  printAll() {
+    if (this.length === 0) {
+      console.log("Nothing in this linked list.");
+      return;
+    }
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+let myLinkedList = new LinkedList();
+myLinkedList.push("Mike");
+myLinkedList.push("Harry");
+myLinkedList.push("James");
+myLinkedList.push("Kevin");
+
+myLinkedList.printAll();
+console.log(myLinkedList.length);
---
+++ Coding Pracitce/Section 9 Data Structure I/76. Linked List Pop.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (!this.head) {
+      return null;
+    } else if (this.length = 1) {
+      let temp = this.head;
+      this.head = null;
+      this.length = 0;
+      return temp;
+    } else {
+      let currentNode = this.head;
+      for (let i = 1; i <= this.length - 2; i++) {
+        currentNode = currentNode.next;
+      }
+      let temp = currentNode.next;
+      currentNode.next = null;
+      this.length--;
+      return temp;
+    }
+  }
+
+  printAll() {
+    if (this.length === 0) {
+      console.log("Nothing in this linked list.");
+      return;
+    }
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+let myLinkedList = new LinkedList();
+myLinkedList.push("Mike");
+myLinkedList.push("Harry");
+myLinkedList.push("James");
+myLinkedList.push("Kevin");
+
+myLinkedList.printAll();
