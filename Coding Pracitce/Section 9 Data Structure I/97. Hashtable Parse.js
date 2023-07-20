546f8147922c82281d3d062269f521f9e11498be
yunhsuan <yunhsuan2017@gmail.com>
2023-07-20 16:20:04 +0800

    97

--- Coding Pracitce/Section 9 Data Structure I/97. Hashtable Parse.js
+++ Coding Pracitce/Section 9 Data Structure I/97. Hashtable Parse.js
@@ -3,16 +3,36 @@ class Hashtable {
     this.size = size;
     this.table = [];
     for (let i = 0; i < this.size; i++) {
-      this.table.push([]);
-      //  Array<Array> ,[[],[],[]....]
+      this.table.push([]);//  Array<Array> ,[[],[],[]....]
     }
   }
+
+  parse(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+      result += str.charCodeAt(i);
+    }
+    return result % this.size;
+  }
+
   // division method
   hash1(key) {
+    let parsedKey = 0;
+    if (typeof key !== "Number") {
+      parsedKey = this.parse(key);
+    } else {
+      parsedKey = key
+    }
     return key % this.size;
   }
   // multiplication method
   hash2(key) {
+    let parsedKey = 0;
+    if (typeof key !== "Number") {
+      parsedKey = this.parse(key);
+    } else {
+      parsedKey = key
+    }
     const A = (Math.sqrt(5) - 1) / 2;
     return Math.floor(this.size * ((key * A) % 1));
   }
@@ -38,9 +58,9 @@ class Hashtable {
 }
 
 let myHashTable = new HashTable(6);
-myHashTable.set(11424, "Mike");
-myHashTable.set(6254, "James");
-myHashTable.set(4171, "Drake");
-myHashTable.set(544, "Kevin");
+// CSS
+myHashTable.set("white", "#FFFFFF")
+myHashTable.set("magenta", "#FF00FF")
+myHashTable.set("red", "#FF0000")
 
 myHashTable.printAll();
