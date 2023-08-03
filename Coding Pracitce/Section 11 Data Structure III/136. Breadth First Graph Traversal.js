2c5b1fba7f386c4903fbef7e1fdd704546d4f03f
yunyunfunnydays <yunhsuan2017@gmail.com>
2023-08-03 15:26:46 +0800

    136

---
+++ Coding Pracitce/Section 11 Data Structure III/136. Breadth First Graph Traversal.js
@@ -0,0 +1,100 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.neighbors = [];
+    this.visited = false;
+  }
+
+  addNeighbor(node) {
+    this.neighbors.push(node);
+  }
+}
+
+
+let result = [];
+function DFT(starter) {
+  starter.visited = true;
+  result.push(starter.value);
+
+  starter.neighbors.forEach((node) => {
+    if (!node.visited) {
+      DFT(node);
+    }
+  })
+  return result;
+}
+
+function BFT(starter) {
+  let q = [];
+  if (starter) {
+    q.push(starter);
+    starter.visited = true;
+    while (q.length > 0) {
+      let newNode = q.shift();
+      result.push(newNode.value);
+      newNode.neighbors.forEach(node => {
+        if (!node.visited) {
+          q.push(node);
+          node.visited = true;
+        }
+      });
+    }
+  }
+  return result;
+}
+
+let A = new Node("A");
+let B = new Node("B");
+let C = new Node("C");
+let D = new Node("D");
+let E = new Node("E");
+let F = new Node("F");
+let G = new Node("G");
+let H = new Node("H");
+let I = new Node("I");
+let J = new Node("J");
+let K = new Node("K");
+let L = new Node("L");
+let M = new Node("M");
+
+A.addNeighbor(E);
+A.addNeighbor(F);
+B.addNeighbor(D);
+C.addNeighbor(D);
+D.addNeighbor(B);
+D.addNeighbor(C);
+D.addNeighbor(E);
+D.addNeighbor(I);
+E.addNeighbor(A);
+E.addNeighbor(D);
+E.addNeighbor(F);
+E.addNeighbor(I);
+F.addNeighbor(A);
+F.addNeighbor(E);
+F.addNeighbor(I);
+G.addNeighbor(H);
+G.addNeighbor(I);
+H.addNeighbor(G);
+H.addNeighbor(I);
+H.addNeighbor(L);
+I.addNeighbor(D);
+I.addNeighbor(E);
+I.addNeighbor(F);
+I.addNeighbor(G);
+I.addNeighbor(H);
+I.addNeighbor(J);
+I.addNeighbor(K);
+I.addNeighbor(M);
+J.addNeighbor(I);
+J.addNeighbor(M);
+K.addNeighbor(I);
+K.addNeighbor(L);
+K.addNeighbor(M);
+L.addNeighbor(H);
+L.addNeighbor(K);
+M.addNeighbor(I);
+M.addNeighbor(J);
+M.addNeighbor(K);
+
+// console.log(DFT(A));
+console.log(BFT(F));
